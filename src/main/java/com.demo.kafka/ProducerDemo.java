ecc87171c1681222393858cd2ed1135a3a65abc3
Phuong Tran La Nhat <tlnphuong@axonactive.com>
2023-08-25 19:12:50 +0700

    init kafka producer

---
+++ src/main/java/com.demo.kafka/ProducerDemo.java
@@ -0,0 +1,37 @@
+package com.demo.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
+
+    public static void main(String[] args) {
+        log.info("This is a Kafka producer");
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("demojava",1, "second_producer","second message");
+
+        producer.send(producerRecord);
+
+        producer.flush();
+
+        producer.close();
+    }
+}
+
+
